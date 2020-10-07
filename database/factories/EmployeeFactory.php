0b319c30cbae252d374e441cb2cfcfdbdf584912
Marius Kasparavičius <marre124@gmail.com>
2020-10-07 11:44:49 +0300

    Upgrade to Laravel 8

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -1,15 +1,32 @@
 <?php
 
-/* @var $factory \Illuminate\Database\Eloquent\Factory */
+namespace Database\Factories;
 
 use App\Employee;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-$factory->define(Employee::class, function (Faker $faker) {
-    return [
-        'firstname' => $faker->firstName,
-        'lastname' => $faker->lastName,
-        'email' => $faker->unique()->safeEmail,
-        'phonenumber' => $faker->e164PhoneNumber,
-    ];
-});
+class EmployeeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Employee::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'firstname' => $this->faker->firstName,
+            'lastname' => $this->faker->lastName,
+            'email' => $this->faker->unique()->safeEmail,
+            'phonenumber' => $this->faker->e164PhoneNumber,
+        ];
+    }
+}
